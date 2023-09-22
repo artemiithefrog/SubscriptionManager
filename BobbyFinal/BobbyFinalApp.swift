799a6bfcdb61cd63d5dcd4adacdf011977cf911c
Artemii <artemiithefrog95@icloud.com>
2023-09-22 13:36:19 +0200

    Initial Commit

---
+++ BobbyFinal/BobbyFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobbyFinalApp.swift
+//  BobbyFinal
+//
+//  Created by artemiithefrog . on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BobbyFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
