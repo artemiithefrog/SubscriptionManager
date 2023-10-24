c476ba82367a99e04f673d11a77810c9d71c9790
Artemii <artemiithefrog95@icloud.com>
2023-10-24 13:44:21 +0200

    Created func to add custom subscriptions to realmManager

---
+++ BobbyFinal/Views/Custom Subscription/CustomSubscriptionIcon.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSubscriptionIcon.swift
+//  BobbyFinal
+//
+//  Created by artemiithefrog . on 24.10.2023.
+//
+
+import SwiftUI
+
+struct CustomSubscriptionIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSubscriptionIcon()
+}
