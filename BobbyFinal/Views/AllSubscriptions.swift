8e00b2d50b09e2527aa4d8457f1c65a5e204e458
Artemii <artemiithefrog95@icloud.com>
2023-10-24 12:37:55 +0200

    Changed NavigationStack to VStack

--- BobbyFinal/Views/AllSubscriptions.swift
+++ BobbyFinal/Views/AllSubscriptions.swift
@@ -9,68 +9,60 @@ import RealmSwift
 import SwiftUI
 
 struct AllSubscriptions: View {
-
+    
     @StateObject var realmManager = RealmManager()
+    @State private var showAddSubscription = false
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                if realmManager.subscriptions.isEmpty {
-                    VStack {
-                        Spacer()
-                        Text("Hi, my name is Bobby 3!")
-                            .fontWeight(.medium)
-                            .padding(.bottom, -20)
-                        Text("Add your first subscription by tapping the + icon at the top.")
-                            .foregroundColor(.gray)
-                            .frame(width: 250, height: 100)
-                            .multilineTextAlignment(.center)
-                        Spacer()
-                    }
-                } else {
-                    Subscriptions(subscriptiions: realmManager.subscriptions)
-                        .padding(.top, 10)
+        VStack {
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "gear")
                 }
+                .tint(.gray)
+                Spacer()
+                HStack {
+                    Text("All Subscriptions")
+                        .tint(.black)
+                        .fontWeight(.bold)
+                    Image(systemName: "chevron.down")
+                        .resizable()
+                        .frame(width: 14, height: 7)
+                        .fontWeight(.medium)
+                }
+                Spacer()
+                Button {
+                    showAddSubscription.toggle()
+                } label: {
+                    Image(systemName: "plus")
+                        .fontWeight(.medium)
+                }
+                .tint(.gray)
             }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        //                        settings
-                    } label: {
-                        Image(systemName: "gear")
-                    }
-                    .tint(.gray)
+            .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+            if realmManager.subscriptions.isEmpty {
+                VStack {
+                    Spacer()
+                    Text("Hi, my name is Bobby 3!")
+                        .fontWeight(.medium)
+                        .padding(.bottom, -20)
+                    Text("Add your first subscription by tapping the + icon at the top.")
+                        .foregroundColor(.gray)
+                        .frame(width: 250, height: 100)
+                        .multilineTextAlignment(.center)
+                    Spacer()
                 }
-                ToolbarItem(placement: .principal) {
-                    HStack {
-                        Button {
-                            
-                        } label: {
-                            Text("All Subscriptions")
-                                .tint(.black)
-                                .fontWeight(.bold)
-                        }
-                        Image(systemName: "chevron.down")
-                            .resizable()
-                            .frame(width: 14, height: 7)
-                            .fontWeight(.medium)
-                    }
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        realmManager.showAddSubscription.toggle()
-                    } label: {
-                        Image(systemName: "plus")
-                            .fontWeight(.medium)
-                    }
-                    .tint(.gray)
-                }
+            } else {
+                Subscriptions(subscriptiions: realmManager.subscriptions)
+                    .padding(.top, 10)
             }
-            .sheet(isPresented: $realmManager.showAddSubscription) {
-                AddSubscription()
-                    .environmentObject(realmManager)
-            }
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .sheet(isPresented: $showAddSubscription) {
+            AddSubscription()
+                .environmentObject(realmManager)
         }
     }
 }
