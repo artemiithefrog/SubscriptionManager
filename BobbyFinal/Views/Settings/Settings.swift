0fe33f75e3ef16659a18117f77ed6a63782c61c8
Artemii <artemiithefrog95@icloud.com>
2023-11-02 06:55:06 +0100

    Created Settings view design

--- BobbyFinal/Views/Settings/Settings.swift
+++ BobbyFinal/Views/Settings/Settings.swift
@@ -34,6 +34,98 @@ struct Settings: View {
             .padding()
             
             ScrollView {
+                Text("GENERAL")
+                    .foregroundColor(.gray)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 13))
+                Divider()
+                Text("Security")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 15))
+                    .onTapGesture {
+                        
+                    }
+                Divider()
+                Text("ICloud Sync")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 15))
+                    .onTapGesture {
+                        
+                    }
+                Divider()
+                Text("MY SUBSCRIPTIONS")
+                    .foregroundColor(.gray)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.top, 10)
+                    .font(.system(size: 13))
+                Divider()
+                Text("Sort Subscriptions by")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 15))
+                    .onTapGesture {
+                        
+                    }
+                Divider()
+                Text("View Total as")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 15))
+                    .onTapGesture {
+                        
+                    }
+                Divider()
+                Text("Default Currency")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 15))
+                    .onTapGesture {
+                        
+                    }
+                Divider()
+                Text("DESIGN")
+                    .foregroundColor(.gray)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.top, 10)
+                    .font(.system(size: 13))
+                Divider()
+                Text("Sort Subscriptions by")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 15))
+                    .onTapGesture {
+                        
+                    }
+                Divider()
+                Text("View Total as")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 15))
+                    .onTapGesture {
+                        
+                    }
+                Divider()
+                Text("Default Currency")
+                    .foregroundColor(.black)
+                    .frame(width: UIScreen.main.bounds.width, alignment: .leading)
+                    .padding(.leading)
+                    .font(.system(size: 15))
+                    .onTapGesture {
+                        
+                    }
+                Divider()
                 
             }
         }
