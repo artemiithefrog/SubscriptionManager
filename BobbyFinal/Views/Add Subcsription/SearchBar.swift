e0adc8ec513cfb894be7072aac2084021c55ea48
Artemii <artemiithefrog95@icloud.com>
2023-11-04 22:30:24 +0100

    Created SearchBar view

---
+++ BobbyFinal/Views/Add Subcsription/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  BobbyFinal
+//
+//  Created by artemiithefrog . on 04.11.2023.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
