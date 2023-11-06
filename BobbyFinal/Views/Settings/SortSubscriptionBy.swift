6976604e6bd6843536cd2e1f19b88d7cc78b9a75
Artemii <artemiithefrog95@icloud.com>
2023-11-06 20:04:05 +0100

    Made SortSubscriptionBy view

--- BobbyFinal/Views/Settings/SortSubscriptionBy.swift
+++ BobbyFinal/Views/Settings/SortSubscriptionBy.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct SortSubscriptionBy: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var subscriptionSort = ["Latest", "Alphabetical (A - Z)", "Alphabetical (Z - A)", "Color", "Payment Date", "Price (High - Low)", "Price (Low -High)", "None"]
+    @State private var selectedSort = "None"
+    
     var body: some View {
-        Text("Sort Subscription By")
+        NavigationStack {
+            HStack {
+                ZStack {
+                    Text("Subscription Sort Order")
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                    HStack {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image(systemName: "chevron.left")
+                                .resizable()
+                                .frame(width: 10, height: 17)
+                        }.tint(.gray)
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            ScrollView {
+                ForEach(subscriptionSort, id: \.self) { order in
+                    VStack {
+                        Divider()
+                        HStack {
+                            Text(order)
+                            Spacer()
+                        }
+                        .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+                        Divider()
+                    }
+                }
+            }
+        }
+        .navigationBarBackButtonHidden(true)
     }
 }
 
